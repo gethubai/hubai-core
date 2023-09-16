e7ec0e041d71f35be794ae87bec5bd510bfc6697
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-16 14:31:21 -0300

    add brain and extension properties to ISettings

--- src/model/settings.ts
+++ src/model/settings.ts
@@ -11,25 +11,43 @@ export enum SettingsEvent {
   OnSave = 'settings.onsave',
 }
 
+export interface IExtensionsConfiguration {
+  [index: string]: any;
+}
+
+export interface IBrainConfiguration {
+  [index: string]: any;
+}
+
 export interface ISettings {
   colorTheme?: string;
   editor?: IEditorOptions;
   locale?: string;
+  extension: IExtensionsConfiguration;
+  brain: IBrainConfiguration;
   [index: string]: any;
 }
 
 export class SettingsModel implements ISettings {
   colorTheme: string;
-
   editor: IEditorOptions;
-
   locale?: string;
+  extension: IExtensionsConfiguration;
+  brain: IBrainConfiguration;
+  [key: string]: any;
 
-  constructor(colorTheme: string, editor: IEditorOptions, locale?: string) {
+  constructor(
+    colorTheme: string,
+    editor: IEditorOptions,
+    locale?: string,
+    extension?: IExtensionsConfiguration,
+    brain?: IBrainConfiguration
+  ) {
     this.colorTheme = colorTheme;
     this.editor = editor;
     this.locale = locale;
+    this.extension = extension ?? {};
+    this.brain = brain ?? {};
   }
 
-  [key: string]: any;
 }
